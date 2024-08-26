524d9ff52e88f6241e65dfe4d6cc9e45ecc48617
Andrii1000 <romaniyka9@gmail.com>
2024-08-26 09:28:10 +0300

    Solution

---
+++ app/serializers.py
@@ -0,0 +1,26 @@
+import json
+from abc import abstractmethod, ABC
+from xml.etree.ElementTree import Element, SubElement, tostring
+
+from app.book import Book
+
+
+class SerializerBase(ABC):
+    @abstractmethod
+    def serializer(self, book: Book) -> str:
+        pass
+
+
+class JsonSerializer(SerializerBase):
+    def serializer(self, book: Book) -> str:
+        return json.dumps({"title": book.title, "content": book.content})
+
+
+class XmlSerializer(SerializerBase):
+    def serializer(self, book: Book) -> str:
+        root = Element("book")
+        title = SubElement(root, "title")
+        title.text =book.title
+        content = SubElement(root, "content")
+        content.text = book.content
+        return tostring(root, encoding="unicode")
